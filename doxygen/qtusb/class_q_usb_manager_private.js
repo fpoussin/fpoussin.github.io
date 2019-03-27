499ecd27443d611830b9b25517fe433d02831558
Fabien Poussin <fabien.poussin@gmail.com>
2019-03-27 19:52:35 +0100

    Remove old files

--- doxygen/qtusb/class_q_usb_manager_private.js
+++
@@ -1,8 +0,0 @@
-var class_q_usb_manager_private =
-[
-    [ "QUsbManagerPrivate", "class_q_usb_manager_private.html#a002512c4919d06302aaf19519761af84", null ],
-    [ "~QUsbManagerPrivate", "class_q_usb_manager_private.html#a3fb60a546bff2b063419652580bee391", null ],
-    [ "m_ctx", "class_q_usb_manager_private.html#a0c40667fb363fc43610465d00073eb59", null ],
-    [ "m_has_hotplug", "class_q_usb_manager_private.html#a2df797e53e407435dcd3a8852b465b5b", null ],
-    [ "m_refresh_timer", "class_q_usb_manager_private.html#a2a89670707c00d29c68dd827dc30c490", null ]
-];
